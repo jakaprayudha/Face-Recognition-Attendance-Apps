83dff0221f216648ca37ae9728d26ec9632e92ad
jakaprayudha <jakaprayudha3@gmail.com>
2025-10-08 17:21:26 +0700

    feat .env connection database

--- db/connect.py
+++ db/connect.py
@@ -1,9 +1,20 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+# Muat variabel dari file .env
+load_dotenv()
 
 def get_connection():
-    return mysql.connector.connect(
-        host="localhost",     # ganti sesuai host
-        user="root",          # user database
-        password="",          # password database
-        database="face_absensi"
-    )
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            autocommit=True
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"[DB ERROR] {err}")
+        return None
